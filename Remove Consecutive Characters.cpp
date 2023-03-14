306819a8f24f44b8324bf635646d517a135be36e
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-03-14 22:01:59 +0530

    Create Remove Consecutive Characters.cpp

---
+++ Remove Consecutive Characters.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    string removeConsecutiveCharacter(string s)
+    {
+        // code here.
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]==s[i+1])
+            {
+                s.erase(i--,1);
+            }
+        }
+        cout<<s;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.removeConsecutiveCharacter(s)<<endl;
+    }
+} 
+
+
+
+
+// } Driver Code Ends
